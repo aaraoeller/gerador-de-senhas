2c063c7860dfa76fd3c9dba4102c4dfe4a9eb2d4
aarao <89228715+aaraoeller@users.noreply.github.com>
2022-04-26 14:29:10 -0300

    finalizado

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let containerPassword = document.querySelector("#container-password");
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*?";
 let novaSenha = "";
 
-sizePassword.innerHTML = sliderElement.Value;
+sizePassword.innerHTML = sliderElement.value;
 
 slider.oninput = function(){
     sizePassword.innerHTML = this.value;
@@ -18,6 +18,19 @@ slider.oninput = function(){
 function generatePassword(){
     
 let pass = "";
-for()
 
+for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n))
+
+}
+
+containerPassword.classList.remove("hide");
+password.innerHTML = pass;
+novaSenha = pass;
+
+}
+
+function copyPassword(){
+    alert("Senha copiada")
+    navigator.clipboard.writeText(novaSenha);
 }
